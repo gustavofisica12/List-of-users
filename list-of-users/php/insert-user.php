204718d3bca86864aa5aa2f3a9e08c68b4405f65
Gustavo Carrillo Cervera <72534090+gustavofisica12@users.noreply.github.com>
2022-06-07 21:24:07 -0500

    Add files via upload

---
+++ list-of-users/php/insert-user.php
@@ -0,0 +1,54 @@
+<?php
+
+error_reporting(0);
+header('Content-type: application/json; charset=utf-8');
+
+$name = $_POST['namei'];
+$name = filter_var($name, FILTER_SANITIZE_STRING);
+
+$lastName = $_POST['lastName'];
+$lastName = filter_var($lastName, FILTER_SANITIZE_STRING);
+
+$addres = $_POST['addres'];
+$addres = filter_var($addres, FILTER_SANITIZE_STRING);
+
+$phone = $_POST['phone'];
+$phone = filter_var($phone, FILTER_SANITIZE_STRING);
+
+function validateData($name, $lastName, $addres, $phone) {
+    if ($name == '') {
+        return false;
+    } elseif ($lastName == '') {
+        return false;
+    } elseif ($addres == ''){
+        return false;
+    } elseif ($phone == '') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+if (validateData($name, $lastName, $addres, $phone)) {
+    try {
+        $connection = new mysqli("localhost", "root", "", "list");
+        $connection->set_charset('utf8');
+
+        $statement = $connection->prepare("INSERT INTO `users`(`name`, `last name`, `addres`, `phone`) VALUES(?,?,?,?)");
+        $statement->bind_param("ssss", $name, $lastName, $addres, $phone);
+        $statement->execute();
+
+        $answer = [];
+
+        if ($connection->affected_rows <= 0) {
+            $answer = ['error' => true];
+        }
+        
+    } catch (Exception $e) {
+        $answer = ['error' => true];
+    }
+} else{
+    $answer = ['error' => true];
+}
+
+echo json_encode($answer);
